1cca05db813b2e496d628af4918f61174fe5393d
Tanya Gupta <tanyagupta@Tanyas-MacBook-Air.local>
2018-05-23 17:17:16 -0400

    changes to restaurant.html

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,8 +9,8 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return 'http://127.0.0.1:8000/data/restaurants.json'
-    //return `http://localhost:${port}/data/restaurants.json`;
+    //return 'http://127.0.0.1:8000/data/restaurants.json'
+    return `http://localhost:${port}/data/restaurants.json`;
   }
 
   /**
